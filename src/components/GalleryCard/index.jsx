53a67bed106b4c1253cf889cb89cde5b26d70111
karinahrens <karina.ahrens@gmail.com>
2023-11-07 16:34:09 +0000

    adding styling and edit button

--- src/components/GalleryCard/index.jsx
+++ src/components/GalleryCard/index.jsx
@@ -1,11 +1,21 @@
-export default function GalleryCard({char}) {
+export default function GalleryCard({ char, onEdit, onDelete }) {
+  const handleEditClick = () => {
+    onEdit(char.id); 
+  };
+
+  const handleDeleteClick = () => {
+    onDelete(char.id); 
+  };
+
   return (
     <div className="gallery-image">
       <div>
-          <h2>{char.name}</h2>
-          <span>{char.age}</span>
-          <p>{char.catch_phrase}</p>
+        <h2>{char.name}</h2>
+        <span>{char.age}</span>
+        <p>{char.catch_phrase}</p>
+        <button className="button" onClick={handleEditClick}>Edit</button>
+        <button className="button" onClick={handleDeleteClick}>Delete</button>
       </div>
     </div>
-  )
-};
+  );
+}
